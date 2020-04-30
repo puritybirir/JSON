a8f9216a7f1d3043bcfd2aaca2db616ef0ec5aca
puritybirir <puritybirir@gmail.com>
2020-04-30 17:04:20 +0300

    Object notations

---
+++ index.js
@@ -0,0 +1,18 @@
+var myObj = {
+    firstName: "Mike",
+    lastName: "Luke",
+    Occupation: "Teacher",
+};
+
+console.log(myObj);
+
+// Dot notation
+var myJSON = {};
+myJSON.car1 = "black";
+console.log(myJSON);
+myJSON.car2 = "blue";
+console.log(myJSON);
+
+// Bracket notation
+myJSON["car3"] = "Yellow";
+console.log(myJSON);
