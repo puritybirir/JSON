ab5a37c8f065b3faf0851b64726d5aab44cf155d
puritybirir <puritybirir@gmail.com>
2020-04-30 17:02:57 +0300

    Object creation

---
+++ object.js
@@ -0,0 +1,11 @@
+let output1 = document.getElementById('output1');
+let output2 = document.getElementById('output2');
+
+var myObj = {
+    firstName: "Mike",
+    lastName: "Luke",
+    Occupation: "Teacher",
+};
+
+output1.innerHTML = myObj.firstName;
+output2.innerHTML = myObj.lastName;
